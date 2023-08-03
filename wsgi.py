a84dcb5792307946498c8bcddf627f9aa1e8e36b
Zwitschi <zwitschi82@gmail.com>
2023-08-03 22:39:16 +0200

    adjustments for .env and venv

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,16 @@
+import os
+from dotenv import load_dotenv
 from app import app
 
+# Load environment variables from .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 if __name__ == '__main__':
+    activate_this = os.path.join(os.path.dirname(__file__), 'venv', 'bin', 'activate_this.py')
+    with open(activate_this) as f:
+        exec(f.read(), {'__file__': activate_this})
+
     from waitress import serve
     serve(app, listen='*:5000')
