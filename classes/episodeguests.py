d1098f87259beffca95ce890a26eb0b01fa34d38
Zwitschi <zwitschi82@gmail.com>
2023-09-22 16:49:34 +0200

    syntax fix

--- classes/episodeguests.py
+++ classes/episodeguests.py
@@ -5,79 +5,79 @@ class EpisodeGuests:
         self.guests = []
         self.get_guests()
 
-def clean_title(self):
-    title_mappings = {
-        'Uncle Bob': 'Uncle Bob + ',
-        'Are You Garbage': 'Kevin James Ryan + Henry Foley',
-        'Chad and JT': 'Chad Kroeger + JT Parr',
-        'Howie Mandel meets The Family': 'Howie Mandel + Mom + Dad',
-        'Sarah-Violet & Charles': 'Sarah-Violet Bliss + Charles Rogers',
-        'The "AS WEE SEE IT" Episode': 'Sue Ann Pien + Albert Rutecki',
-        'Sunday Football w/ The Glassmans': 'Mom + Dad + Uncle Bob + Grandma Gloria',
-        'The Glassman Family': 'Mom + Dad',
-        'The Vegas Dads 2.0 + Adam Ray': 'Dad + Cousin Teddy + Marc + Adam Ray',
-        'The Vegas Dads': 'Dad + Cousin Teddy + Marc',
-        'Nobodies': 'Hugh Davidson + Larry Dorf + Rachel Ramras',
-        '@whitneycummings': 'Whitney Cummings'
-    }
+    def clean_title(self):
+        title_mappings = {
+            'Uncle Bob': 'Uncle Bob + ',
+            'Are You Garbage': 'Kevin James Ryan + Henry Foley',
+            'Chad and JT': 'Chad Kroeger + JT Parr',
+            'Howie Mandel meets The Family': 'Howie Mandel + Mom + Dad',
+            'Sarah-Violet & Charles': 'Sarah-Violet Bliss + Charles Rogers',
+            'The "AS WEE SEE IT" Episode': 'Sue Ann Pien + Albert Rutecki',
+            'Sunday Football w/ The Glassmans': 'Mom + Dad + Uncle Bob + Grandma Gloria',
+            'The Glassman Family': 'Mom + Dad',
+            'The Vegas Dads 2.0 + Adam Ray': 'Dad + Cousin Teddy + Marc + Adam Ray',
+            'The Vegas Dads': 'Dad + Cousin Teddy + Marc',
+            'Nobodies': 'Hugh Davidson + Larry Dorf + Rachel Ramras',
+            '@whitneycummings': 'Whitney Cummings'
+        }
 
-    for keyword, replacement in title_mappings.items():
-        if keyword in self.title:
-            self.title = replacement
+        for keyword, replacement in title_mappings.items():
+            if keyword in self.title:
+                self.title = replacement
 
-    title_parts = self.title.split(' - The Sleepover Series:')
-    if len(title_parts) > 1:
-        self.title = title_parts[0]
+        title_parts = self.title.split(' - The Sleepover Series:')
+        if len(title_parts) > 1:
+            self.title = title_parts[0]
 
-    title_parts = self.title.split('(')
-    if len(title_parts) > 1:
-        self.title = title_parts[0]
+        title_parts = self.title.split('(')
+        if len(title_parts) > 1:
+            self.title = title_parts[0]
 
-    title_parts = self.title.split(' aka')
-    if len(title_parts) > 1:
-        self.title = title_parts[0]
+        title_parts = self.title.split(' aka')
+        if len(title_parts) > 1:
+            self.title = title_parts[0]
 
-    # Remove trailing spaces
-    self.title = self.title.rstrip()
+        # Remove trailing spaces
+        self.title = self.title.rstrip()
 
-    remove_list = [
-        f' - #{self.number}',
-        f'- #{self.number}',
-        f' #{self.number}',
-        f' {self.number}',
-        f' -#{self.number}',
-        f'-#{self.number}',
-        f'#{self.number}',
-        str(self.number),
-        ' UNCENSORED',
-        'on TYSO w/ Rick Glassman - ',
-        'on TYSO - ',
-        'on TYSO -',
-        'on TYSO',
-        'on Take Your Shoes Off',
-        'on TYSO w/ Rick Glassman',
-        'w/ Rick Glassman - ',
-        'w/ Rick Glassman',
-        ' w/ The Glassmans - ',
-        ' w/ The Glassmans -',
-        '50th EPISODE!!',
-        '- on TYSO -',
-        '- on TYSO',
-        ' in NY'
-    ]
+        remove_list = [
+            f' - #{self.number}',
+            f'- #{self.number}',
+            f' #{self.number}',
+            f' {self.number}',
+            f' -#{self.number}',
+            f'-#{self.number}',
+            f'#{self.number}',
+            str(self.number),
+            ' UNCENSORED',
+            'on TYSO w/ Rick Glassman - ',
+            'on TYSO - ',
+            'on TYSO -',
+            'on TYSO',
+            'on Take Your Shoes Off',
+            'on TYSO w/ Rick Glassman',
+            'w/ Rick Glassman - ',
+            'w/ Rick Glassman',
+            ' w/ The Glassmans - ',
+            ' w/ The Glassmans -',
+            '50th EPISODE!!',
+            '- on TYSO -',
+            '- on TYSO',
+            ' in NY'
+        ]
 
-    for r in remove_list:
-        self.title = self.title.replace(r, '')
+        for r in remove_list:
+            self.title = self.title.replace(r, '')
 
-    for n in range(1, 50):
-        self.title = self.title.replace(f'{n}.0', str(n))
+        for n in range(1, 50):
+            self.title = self.title.replace(f'{n}.0', str(n))
 
-    # Remove extra spaces
-    self.title = ' '.join(self.title.split())
+        # Remove extra spaces
+        self.title = ' '.join(self.title.split())
 
-    and_list = [' & ', ' and ', ' AND ']
-    for a in and_list:
-        self.title = self.title.replace(a, ' + ')
+        and_list = [' & ', ' and ', ' AND ']
+        for a in and_list:
+            self.title = self.title.replace(a, ' + ')
     
     def get_guests(self):
         # create a list of guests
